dfd7ac370c9a24ccda9e6f15cbacf2730c18acc2
Miraclepio <miraclepio75@gmail.com>
2024-08-11 13:39:16 +0100

    added my render url2

--- controller/userController.js
+++ controller/userController.js
@@ -47,7 +47,7 @@ exports.signUpUser = async (req, res) => {
 
         return res.status(200).json({ message: "Successful, please check your email to verify your account",token ,user});
     } catch (error) {
-         res.status(500).json(error.message);
+         res.status(500).json(error.message); 
     }
 };
 
